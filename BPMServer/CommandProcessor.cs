ef5c189be5509de957a4423711445937138800c1
yyc12345 <yyc12321@outlook.com>
2019-05-30 14:37:21 +0800

    [DO NOT BUILD] fixing BPMServer...

--- BPMServer/CommandProcessor.cs
+++ BPMServer/CommandProcessor.cs
@@ -16,7 +16,7 @@ namespace BPMServer.Command {
         });
 
         public static bool Process(string command) {
-            return parser.ParseArguments<ExitOption, ConfigOption, ModeOption, ClientOption, UpdateOption>(
+            return parser.ParseArguments<ExitOption, ConfigOption, SwitchOption, ClientOption, ImportOption, LsOption, ShowOption, AddpkgOption, EditpkgOption, DelpkgOption, AddverOption, EditverOption, DelverOption>(
                 CommandSplitter.Split(command))
                 .MapResult(
                 (ExitOption opt) => {
@@ -34,7 +34,7 @@ namespace BPMServer.Command {
 
                 },
                 (ConfigOption opt) => {
-                    if (opt.Key is null) 
+                    if (opt.Key is null)
                         Config.Core();
                     else {
                         if (opt.NewValue is null)
@@ -44,39 +44,31 @@ namespace BPMServer.Command {
                     }
                     return false;
                 },
-                (ModeOption opt) => {
-                    if (opt.Mode == "maintain") {
-                        if (General.IsMaintaining) ConsoleAssistance.WriteLine("You are in maintain mode now.", ConsoleColor.Red);
-                        else {
-                            General.CoreTcpProcessor.StopListen();
-                            Console.WriteLine("Waiting the release of resources...");
-                            if (General.ManualResetEventList.Count != 0)
-                                WaitHandle.WaitAll(General.ManualResetEventList.ToArray());
+                (SwitchOption opt) => {
+                    if (!General.IsMaintaining) {
+                        General.CoreTcpProcessor.StopListen();
+                        Console.WriteLine("Waiting the release of resources...");
+                        if (General.ManualResetEventList.Count != 0)
+                            WaitHandle.WaitAll(General.ManualResetEventList.ToArray());
 
-                            General.GeneralDatabase.Open();
+                        General.GeneralDatabase.Open();
 
-                            General.IsMaintaining = true;
-                            ConsoleAssistance.WriteLine("Switch to maintain mode successfully.", ConsoleColor.Yellow);
-                        }
-                    } else if (opt.Mode == "running") {
-                        if (!General.IsMaintaining) ConsoleAssistance.WriteLine("You are in running mode now.", ConsoleColor.Red);
-                        else {
-                            General.GeneralDatabase.Close();
-                            General.CoreTcpProcessor.StartListen();
+                        General.IsMaintaining = true;
+                        ConsoleAssistance.WriteLine("Switch to maintain mode successfully.", ConsoleColor.Yellow);
 
-                            General.IsMaintaining = false;
-                            ConsoleAssistance.WriteLine("Switch to running mode successfully.", ConsoleColor.Yellow);
-                        }
-                    } else ConsoleAssistance.WriteLine("Invalid parameter", ConsoleColor.Red);
+                    } else {
+                        General.GeneralDatabase.Close();
+                        General.CoreTcpProcessor.StartListen();
+
+                        General.IsMaintaining = false;
+                        ConsoleAssistance.WriteLine("Switch to running mode successfully.", ConsoleColor.Yellow);
+
+                    }
                     return false;
                 },
                 (ClientOption opt) => {
                     if (!General.IsMaintaining) ConsoleAssistance.WriteLine($"Current client: {General.ManualResetEventList.Count}", ConsoleColor.Yellow);
                     else ConsoleAssistance.WriteLine("Server is being maintained. There are no any client.", ConsoleColor.Red);
-                    return false;
-                },
-                (UpdateOption opt) => {
-                    ;//todo: need finish update function
                     return false;
                 },
                 errs => { OutputHelp(); return false; });
@@ -90,14 +82,22 @@ namespace BPMServer.Command {
             Console.WriteLine("Most used commands:");
             Console.WriteLine("General command");
             Console.WriteLine("\texit [-f] - exit server. if you choose -f switch, it will kill the server without any hesitation (for emergency). you will lost each of your modifications");
-            Console.WriteLine("\tconfig - edit server config(config will be applied in the next startup)");
-            Console.WriteLine("\tmode [maintain | running] - switch server mode");
+            Console.WriteLine("\tconfig [setting-name] [new-value] - edit server config(config will be applied in the next startup)");
+            Console.WriteLine("\tswitch - switch server mode between maintain and running");
             Console.WriteLine("");
             Console.WriteLine("Running mode command");
             Console.WriteLine("\tclient - list the number of client which is connecting this server");
             Console.WriteLine("");
             Console.WriteLine("Maintain mode command");
-            Console.WriteLine("\tupdate new_database package_folder - update the whole of this server's data");
+            Console.WriteLine("\timport file-path - import a script file to maintain server quickly");
+            Console.WriteLine("\tls condition - list related packages");
+            Console.WriteLine("\tshow full-name - list specific packages' detail");
+            Console.WriteLine("\taddpkg name aka type desc - add a new package");
+            Console.WriteLine("\teditpkg name aka type desc - edit a package (use ~ to keep the original value)");
+            Console.WriteLine("\tdelpkg name - remove a package");
+            Console.WriteLine("\taddver name parent additional-desc timestamp suit-os dependency reverse-conflict conflict require-decompress internal-script hash package-path - add a new version");
+            Console.WriteLine("\teditver name parent additional-desc timestamp suit-os dependency reverse-conflict conflict require-decompress internal-script hash package-path - edit a version (use ~ to keep the original value)");
+            Console.WriteLine("\tdelver name - remove a version");
             Console.WriteLine("");
             Console.WriteLine("Glory to BKT.");
         }
@@ -119,10 +119,9 @@ namespace BPMServer.Command {
         public string NewValue { get; set; }
     }
 
-    [Verb("mode")]
-    public class ModeOption {
-        [Value(0, Required = true)]
-        public string Mode { get; set; }
+    [Verb("switch")]
+    public class SwitchOption {
+
     }
 
     [Verb("client")]
@@ -130,12 +129,114 @@ namespace BPMServer.Command {
 
     }
 
-    [Verb("update")]
-    public class UpdateOption {
+    [Verb("import")]
+    public class ImportOption {
         [Value(0, Required = true)]
-        public string NewDatabasePath { get; set; }
+        public string FilePath { get; set; }
+    }
+
+    [Verb("ls")]
+    public class LsOption {
+        [Value(0)]
+        public string Condition { get; set; }
+    }
+
+    [Verb("show")]
+    public class ShowOption {
+        [Value(0, Required = true)]
+        public string FullPackageName { get; set; }
+    }
+
+    [Verb("addpkg")]
+    public class AddpkgOption {
+        [Value(0, Required = true)]
+        public string Name { get; set; }
         [Value(1, Required = true)]
-        public string PackageFolderPath { get; set; }
+        public string Aka { get; set; }
+        [Value(2, Required = true)]
+        public int Type { get; set; }
+        [Value(3, Required = true)]
+        public string Desc { get; set; }
+    }
+
+    [Verb("editpkg")]
+    public class EditpkgOption {
+        [Value(0, Required = true)]
+        public string Name { get; set; }
+        [Value(1, Required = true)]
+        public string Aka { get; set; }
+        [Value(2, Required = true)]
+        public int Type { get; set; }
+        [Value(3, Required = true)]
+        public string Desc { get; set; }
+    }
+
+    [Verb("delpkg")]
+    public class DelpkgOption {
+        [Value(0, Required = true)]
+        public string Name { get; set; }
+    }
+
+    [Verb("addver")]
+    public class AddverOption {
+        [Value(0, Required = true)]
+        public string Name { get; set; }
+        [Value(1, Required = true)]
+        public string Parent { get; set; }
+        [Value(2, Required = true)]
+        public string AdditionalDesc { get; set; }
+        [Value(3, Required = true)]
+        public long Timestamp { get; set; }
+        [Value(4, Required = true)]
+        public int SuitOS { get; set; }
+        [Value(5, Required = true)]
+        public string Dependency { get; set; }
+        [Value(6, Required = true)]
+        public string ReverseConflict { get; set; }
+        [Value(7, Required = true)]
+        public string Conflict { get; set; }
+        [Value(8, Required = true)]
+        public string RequireDecompress { get; set; }
+        [Value(9, Required = true)]
+        public string InternalScript { get; set; }
+        [Value(10, Required = true)]
+        public string HASH { get; set; }
+        [Value(11, Required = true)]
+        public string PackagePath { get; set; }
+    }
+
+    [Verb("editver")]
+    public class EditverOption {
+        [Value(0, Required = true)]
+        public string Name { get; set; }
+        [Value(1, Required = true)]
+        public string Parent { get; set; }
+        [Value(2, Required = true)]
+        public string AdditionalDesc { get; set; }
+        [Value(3, Required = true)]
+        public long Timestamp { get; set; }
+        [Value(4, Required = true)]
+        public int SuitOS { get; set; }
+        [Value(5, Required = true)]
+        public string Dependency { get; set; }
+        [Value(6, Required = true)]
+        public string ReverseConflict { get; set; }
+        [Value(7, Required = true)]
+        public string Conflict { get; set; }
+        [Value(8, Required = true)]
+        public string RequireDecompress { get; set; }
+        [Value(9, Required = true)]
+        public string InternalScript { get; set; }
+        [Value(10, Required = true)]
+        public string HASH { get; set; }
+        [Value(11, Required = true)]
+        public string PackagePath { get; set; }
+    }
+
+    [Verb("delpkg")]
+    public class DelverOption {
+        [Value(0, Required = true)]
+        public string Name { get; set; }
     }
 
     #endregion
--- BPMServer/Program.cs
+++ BPMServer/Program.cs
@@ -14,15 +14,45 @@ namespace BPMServer {
 
         static void Main(string[] args) {
 
+            #region comfirm necessary file
+            ConsoleAssistance.WriteLine("Server is checking necessary files...", ConsoleColor.Yellow);
+
+            //package storage
             if (!Directory.Exists(Information.WorkPath.Enter("package").Path))
                 Directory.CreateDirectory(Information.WorkPath.Enter("package").Path);
 
+            var databasePath = Information.WorkPath.Enter("package.db").Path;
+            var rsaPublic = Information.WorkPath.Enter("pub.key").Path;
+            var rsaPrivate = Information.WorkPath.Enter("pri.key").Path;
+            var verifyCache= Information.WorkPath.Enter("verify.dat").Path
+
             //detect database
-            if (!File.Exists(Information.WorkPath.Enter("package.db").Path)) {
+            if (!File.Exists(databasePath)) {
+                ConsoleAssistance.WriteLine("No existing database file. A empty database file will be created.", ConsoleColor.Yellow);
                 General.GeneralDatabase.Open();
                 General.GeneralDatabase.Close();
             }
-            
+
+            //detect encrypt file
+            if(!File.Exists(rsaPublic) || !File.Exists(rsaPrivate)) {
+                //ensure there are no file.
+                ConsoleAssistance.WriteLine("No existing RSA key. A new RSA key will be created.", ConsoleColor.Yellow);
+                File.Delete(rsaPublic);
+                File.Delete(rsaPrivate);
+
+                SignVerifyHelper.GenerateKey(rsaPublic, rsaPrivate);
+            }
+
+            //detect database verify cache
+            if (!File.Exists(verifyCache)) {
+                ConsoleAssistance.WriteLine("No verify cache file. A new cache file will be created.", ConsoleColor.Yellow);
+
+                var str = Convert.ToBase64String(SignVerifyHelper.SignData(databasePath, rsaPublic));
+                File.WriteAllText(verifyCache, str);
+            }
+
+            #endregion
+
             var config = Config.Read();
             General.CoreFileReader = new FileReaderManager();
             General.CoreTcpProcessor = new TcpProcessor(int.Parse(config["IPv4Port"]), int.Parse(config["IPv6Port"]));
